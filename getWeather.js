5e3be8f922a810de92c6104af3f13fa6593674c2
reesef <reesef@gmail.com>
2013-08-20 23:17:35 -0700

    Start building the bg color switch

--- getWeather.js
+++ getWeather.js
@@ -14,10 +14,44 @@ $(document).ready(function() {
             
          if (json.success) {
                
+            // Save the weather objec in variable "currentWeather"
             var currentWeather = json.response.ob;
+
+            // Save the weather description in a variable
+            var conditions = currentWeather.weather.toLowerCase();
+
+            // Save the weather code in a variable
+            var wpc = currentWeather.weatherPrimaryCoded;
+            
+            // Use a switch to set the background based on weather codes
+            switch (wpc) {
+               // Clear
+               case ":CL":
+                  $('.main').addClass('sunny');
+               break;
+
+               // Fair + partly cloudy
+               case ":FW":
+                  $('.main').addClass('partly-cloudy');
+               break;
+               case ":SC":
+                  $('.main').addClass('partly-cloudy');
+               break;
                
+               // Mostly cloudy + cloudy
+               case ":BK":
+                  $('.main').addClass('partly-cloudy');
+               break;
+               case ":OV":
+                  $('.main').addClass('partly-cloudy');
+               break;
+
+               }
+            */
+
+            // Display current temp and ob   
             $('#temp').html(currentWeather.tempF + '\u00B0');               
-            $('#conditions').html(currentWeather.weather.toLowerCase());
+            $('#conditions').html(conditions);
                
             } else {
                   
@@ -39,7 +73,7 @@ $(document).ready(function() {
 
                // Show avgTemp for tomorrow
                $('#tomorrow').html(fc[0].periods[0].avgTempF + '\u00B0');
-               $('#tomorrow').append('<span class=tomorrow>' + 'tomorrow' + '</span>');
+               // $('#tomorrow').append('<span class=tomorrow>' + 'tomorrow' + '</span>');
                
                // Iterate through avgTemp for rest of week
                for (i = 1; i < fc[0].periods.length; i++) {
