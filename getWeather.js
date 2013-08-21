a6a768882e3276637100c235c6b8695655bf7b31
reesef <reesef@gmail.com>
2013-08-20 22:32:05 -0700

    Clean up JS whitespace

--- getWeather.js
+++ getWeather.js
@@ -16,8 +16,7 @@ $(document).ready(function() {
                
             var currentWeather = json.response.ob;
                
-            $('#temp').html(currentWeather.tempF + '\u00B0');
-               
+            $('#temp').html(currentWeather.tempF + '\u00B0');               
             $('#conditions').html(currentWeather.weather.toLowerCase());
                
             } else {
@@ -37,17 +36,19 @@ $(document).ready(function() {
             if (json.success) {
                
                var fc = json.response;
+
+               // Show avgTemp for tomorrow
+               $('#tomorrow').html(fc[0].periods[0].avgTempF + '\u00B0');
+               $('#tomorrow').append('<span class=tomorrow>' + 'tomorrow' + '</span>');
                
-               for (i=0; i < fc[0].periods.length; i++) {
+               // Iterate through avgTemp for rest of week
+               for (i = 1; i < fc[0].periods.length; i++) {
                   
                   $('#forecast').append('<div class=day' + '>' + '<span>' + fc[0].periods[i].avgTempF + '\u00B0' + '</span>' + '</div>');
                
                }
-
             } else {
-               
                alert('Unable to load forecast:' + json.error.description);
-            
             }
          }
       });
