f7065d33fc5c1af96464cdd29091df609ca9647c
reesef <reesef@gmail.com>
2013-08-21 23:41:43 -0700

    Add all the weather codes

--- weather.js
+++ weather.js
@@ -26,28 +26,81 @@ $(document).ready(function() {
             // Use a switch to set the background based on weather codes
             switch (wpc) {
                // Clear
-               case ":CL":
+               case "CL":
                   $('.main').addClass('sunny');
                break;
 
                // Fair + partly cloudy
-               case ":FW":
+               case "FW":
                   $('.main').addClass('partly-cloudy');
                break;
-               case ":SC":
+               case "SC":
                   $('.main').addClass('partly-cloudy');
                break;
                
                // Mostly cloudy + cloudy
-               case ":BK":
+               case "BK":
                   $('.main').addClass('partly-cloudy');
                break;
-               case ":OV":
+               case "OV":
                   $('.main').addClass('partly-cloudy');
                break;
 
+               /*
+               // Fog
+               BR Mist
+               H  Haze
+               F  Fog
+               IF Ice fog
+               ZF Freezing fog
+
+               // Rain
+               L  Drizzle 
+               ZL Freezing drizzle
+               RW Rain showers
+               R  Rain
+               ZR Freezing rain
+               RS Rain/snow mix
+
+               // Hail
+               A  Hail
+               IP Ice pellets / sleet
+
+               // Snow
+               SI Snow/sleet mix
+               WM Wintry mix  
+               S  Snow
+               SW Snow showers
+               ZY Freezing spray
+
+               // Dust/Sand
+               BD Blowing dust   
+               BN Blowing sand
+
+               // Snow
+               BS Blowing snow
+
+               // Frost
+               FR Frost   
+               IC Ice crystals      
+
+               // Smoke
+               K  Smoke
+   
+               // Thunderstorm
+               T  Thunderstorms    
+
+               // Volcanic Ash
+               VA Volcanic ash
+               
+               // Water spouts
+               WP Water spouts   
+ 
+               // Unkown
+               UP Unknown Precipitation   
+               */
                }
-            */
+            
 
             // Display current temp and conditions   
             $('#temp').html(currentWeather.tempF + '\u00B0');               
