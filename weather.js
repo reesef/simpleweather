de00a7903f46f302b727e3c7be7e50c5a9b3f289
reesef <reesef@gmail.com>
2013-08-21 23:03:39 -0700

    Remove the forecast function from weather

--- weather.js
+++ weather.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
                }
             */
 
-            // Display current temp and ob   
+            // Display current temp and conditions   
             $('#temp').html(currentWeather.tempF + '\u00B0');               
             $('#conditions').html(conditions);
                
@@ -57,32 +57,6 @@ $(document).ready(function() {
                   
                alert('Unable to load current conditions: ' + json.error.description);
                
-            }
-         }
-      });
-
-	   // Get the 7 day forecast   
-      $.ajax({
-         url: 'http://api.aerisapi.com/forecasts/' + zip + ' ?client_id=LI7gJnbLEdzpjWXZzBaMP&client_secret=4wgCjhk2sd0AC0YPbzsnp94X1HaY0EdSocIfwSEw',
-         dataType: "jsonp",
-         success: function(json) {
-            
-            if (json.success) {
-               
-               var fc = json.response;
-
-               // Show avgTemp for tomorrow
-               $('#tomorrow').html(fc[0].periods[0].avgTempF + '\u00B0');
-               // $('#tomorrow').append('<span class=tomorrow>' + 'tomorrow' + '</span>');
-               
-               // Iterate through avgTemp for rest of week
-               for (i = 1; i < fc[0].periods.length; i++) {
-                  
-                  $('#forecast').append('<div class=day' + '>' + '<span>' + fc[0].periods[i].avgTempF + '\u00B0' + '</span>' + '</div>');
-               
-               }
-            } else {
-               alert('Unable to load forecast:' + json.error.description);
             }
          }
       });
